bfba5337395c0ab4d769c03e3ea3ab3798fd4367
josrot <josht2838@gmail.com>
2024-08-25 16:28:48 -0400

    print address of listener

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use zero2prod::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    println!("Server running on: {}", &listener.local_addr().unwrap());
     run(listener)?.await
 }
